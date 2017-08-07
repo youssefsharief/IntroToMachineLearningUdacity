b0d97e7350531af63609932c0be568bd486cfd95
Youssef <youssef.sharief@thoughtdesign.com.au>
2017-08-07 11:19:00 +0200

    Decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -19,13 +19,21 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
+clf = DecisionTreeClassifier(min_samples_split=40)
+t0 = time()
+print("Le", len(features_train[0]))
+
+clf.fit(features_train, labels_train)
+print ("training time:", round(time()-t0, 3), "s")
+t0 = time()
+predictions = clf.predict(features_test)
+unique, counts = np.unique(predictions, return_counts=True)
+print(dict(zip(unique, counts)))
 
 
+print ("Predicting time:", round(time()-t0, 3), "s")
 
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
+print ("Score: ---- : ", clf.score(features_test, labels_test))
 
