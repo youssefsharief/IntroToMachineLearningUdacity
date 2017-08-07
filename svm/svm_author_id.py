05afab5ce3a4e51b57eda64f61f15734bea303eb
Youssef <youssef.sharief@thoughtdesign.com.au>
2017-08-07 11:18:51 +0200

    SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -22,9 +22,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### make sure you use // when dividing for integer division
 
 
-#########################################################
-### your code goes here ###
-
-#########################################################
+features_train = features_train[:len(features_train)//100] 
+labels_train = labels_train[:len(labels_train)//100] 
 
 
+
+from sklearn.svm import SVC  
+import numpy as np
+clf = SVC(kernel="rbf", C=10000.0)
+t0 = time()
+clf.fit(features_train, labels_train)
+print ("training time:", round(time()-t0, 3), "s")
+
+t0 = time()
+predictions = clf.predict(features_test)
+unique, counts = np.unique(predictions, return_counts=True)
+print(dict(zip(unique, counts)))
+
+
+print ("Predicting time:", round(time()-t0, 3), "s")
+
+print ("Score: ---- : ", clf.score(features_test, labels_test))
